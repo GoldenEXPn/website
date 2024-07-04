3b239f03218c50c718d818a5b3b7d77a4bde7a60
GoldenEXPn <zty1378212047@gmail.com>
2024-07-04 16:53:48 +0800

    Working on AllAuth

---
+++ test_google_allauth/users/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def logout_view(request):
+    return redirect('/')
