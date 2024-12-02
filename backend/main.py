86757daaf93d5e87296d515157d933008157f81e
Ke Xu <kxu27@u.rochester.edu>
2024-12-02 16:47:53 -0500

    make sure the email is still working

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+# https://developers.google.com/identity/protocols/oauth2/web-server#python_3
+
 import os
 from flask import session, abort, redirect, request
 import flask
@@ -10,15 +12,12 @@ import googleapiclient.discovery
 import cachecontrol
 from google_auth_oauthlib.flow import Flow
 from functools import wraps
+
+from flask_cors import CORS
+
 from bs4 import BeautifulSoup
 import re
-
 import base64
-import html
-
-database_url = os.getenv("DATABASE_URL")
-
-load_dotenv()
 
 CLIENT_SECRETS_FILE = "client_secret.json"
 
@@ -31,15 +30,23 @@ SCOPES = [
 API_SERVICE_NAME = "drive"
 API_VERSION = "v2"
 
-
 app = flask.Flask(__name__)
 
+load_dotenv()
+
+database_url = os.getenv("DATABASE_URL")
 app.secret_key = os.getenv("GOOGLE_OAUTH_CLIENT_SECRET")
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_OAUTH_CLIENT_ID")
 client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
 
 
+CORS(app)
+
 def login_is_required(function):
+    '''
+        Function wrapper that checks if the section is available via id
+        If not, abort the section and provide 401 error
+    '''
      # Preserve the original function's name and metadata
     @wraps(function)
     def wrapper(*args, **kwargs):
@@ -50,9 +57,15 @@ def login_is_required(function):
 
     return wrapper
 
-
 @app.route("/authorize")
 def authorize():
+    '''
+        Get the client secrets file and all of the Id and secret verification 
+        and lead user to authorize their account in google oauth page.
+        After authorization, the oauth would redirect the user to the assigned callback page,
+        which is the callback route, 
+        with desired state id (for verification) 
+    '''
 
     flow = Flow.from_client_secrets_file(
         client_secrets_file=client_secrets_file,
@@ -68,9 +81,16 @@ def authorize():
 
 #TODO: provide check authorization and fetch api
 
-
-@app.route("/callback")
+@app.route("/callback/")
 def callback():
+    
+    '''
+        Get the state value from the stored session and verify it through Google oauth,
+        Then this generate credential for accessing user and api use based on the granted scope.
+        
+        This contains both method of flask default session storage and the cache storage
+    '''
+    
     state = flask.session['state']
 
     flow = Flow.from_client_secrets_file(
@@ -96,13 +116,16 @@ def callback():
 
     session["google_id"] = id_info.get("sub")
     session["name"] = id_info.get("name")
-    
-        
+      
     return redirect("/protected_area")
-
 
 @app.route("/logout")
 def logout():
+    
+    '''
+       Clear session and log out
+    '''
+
     session.clear()
     return redirect("/")
 
@@ -111,23 +134,30 @@ def logout():
 def index():
     return "Hello World <a href='/authorize'><button>Login</button></a>"
 
-
 # @app.route("/")
 # def index():
 #     return print_index_table()
 
-
 @app.route("/protected_area")
-@login_is_required
 def protected_area():
+    if 'credentials' not in flask.session:
+        return flask.redirect('authorize')
+    
     return (
         f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
     )
 
 #API
 @app.route('/emails')
-@login_is_required
 def fetch_emails(): 
+    
+    '''
+        Get the state value from the stored session and verify it through Google oauth,
+        Then this generate credential for accessing user and api use based on the granted scope.
+        
+        This contains both method of flask default session storage and the cache storage
+    '''
+    
     if 'credentials' not in flask.session:
         return flask.redirect('authorize')
 
@@ -196,6 +226,7 @@ def clean_email_body(raw_body):
     # Remove extra newlines and carriage returns (\n, \r)
     clean_body = raw_body.replace("\r", " ").replace("\n", " ")
 
+    clean_body = clean_body.replace("\u200c", " ").replace("\u00a9", " ")
     # Use BeautifulSoup to remove HTML tags
     soup = BeautifulSoup(clean_body, 'html.parser')
     text = soup.get_text(separator=" ", strip=True)  # Extract text without tags
@@ -215,8 +246,7 @@ def clean_email_body(raw_body):
 def clear_credentials():
   if 'credentials' in flask.session:
     del flask.session['credentials']
-  return ('Credentials have been cleared.<br><br>' +
-          print_index_table())
+  return ('Credentials have been cleared.<br><br>')
      
 
 def credentials_to_dict(credentials):
@@ -235,29 +265,28 @@ def check_granted_scopes(credentials):
     features['email'] = False
   return features
 
-
-def print_index_table():
-    return (
-        "<table>"
-        + '<tr><td><a href="/test">Test an API request</a></td>'
-        + "<td>Submit an API request and see a formatted JSON response. "
-        + "    Go through the authorization flow if there are no stored "
-        + "    credentials for the user.</td></tr>"
-        + '<tr><td><a href="/authorize">Test the auth flow directly</a></td>'
-        + "<td>Go directly to the authorization flow. If there are stored "
-        + "    credentials, you still might not be prompted to reauthorize "
-        + "    the application.</td></tr>"
-        + '<tr><td><a href="/revoke">Revoke current credentials</a></td>'
-        + "<td>Revoke the access token associated with the current user "
-        + "    session. After revoking credentials, if you go to the test "
-        + "    page, you should see an <code>invalid_grant</code> error."
-        + "</td></tr>"
-        + '<tr><td><a href="/clear">Clear Flask session credentials</a></td>'
-        + "<td>Clear the access token currently stored in the user session. "
-        + '    After clearing the token, if you <a href="/test">test the '
-        + "    API request</a> again, you should go back to the auth flow."
-        + "</td></tr></table>"
-    )
+# def print_index_table():
+#     return (
+#         "<table>"
+#         + '<tr><td><a href="/test">Test an API request</a></td>'
+#         + "<td>Submit an API request and see a formatted JSON response. "
+#         + "    Go through the authorization flow if there are no stored "
+#         + "    credentials for the user.</td></tr>"
+#         + '<tr><td><a href="/authorize">Test the auth flow directly</a></td>'
+#         + "<td>Go directly to the authorization flow. If there are stored "
+#         + "    credentials, you still might not be prompted to reauthorize "
+#         + "    the application.</td></tr>"
+#         + '<tr><td><a href="/revoke">Revoke current credentials</a></td>'
+#         + "<td>Revoke the access token associated with the current user "
+#         + "    session. After revoking credentials, if you go to the test "
+#         + "    page, you should see an <code>invalid_grant</code> error."
+#         + "</td></tr>"
+#         + '<tr><td><a href="/clear">Clear Flask session credentials</a></td>'
+#         + "<td>Clear the access token currently stored in the user session. "
+#         + '    After clearing the token, if you <a href="/test">test the '
+#         + "    API request</a> again, you should go back to the auth flow."
+#         + "</td></tr></table>"
+#     )
 
 
 if __name__ == "__main__":
